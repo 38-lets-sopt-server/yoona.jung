86f03fe2f852b1de3f918528fd8b59f526b6b862
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-04-08 02:24:44 +0900

    feat: 유효성 검증 로직 별도 분리

--- src/main/java/org/sopt/post/service/PostService.java
+++ src/main/java/org/sopt/post/service/PostService.java
@@ -1,6 +1,7 @@
 package org.sopt.post.service;
 
 import org.sopt.global.exception.PostNotFoundException;
+import org.sopt.global.validation.PostValidator;
 import org.sopt.post.domain.Post;
 import org.sopt.post.dto.request.CreatePostRequest;
 import org.sopt.post.dto.response.CreatePostResponse;
@@ -12,24 +13,23 @@ import java.util.stream.Collectors;
 
 public class PostService {
     private final PostRepository postRepository = new PostRepository();
+    private final PostValidator postValidator = new PostValidator();
 
     // CREATE
     public CreatePostResponse createPost(CreatePostRequest request) {
-        if (request.title == null || request.title.isBlank()) {
-            throw new IllegalArgumentException("제목은 필수입니다!");
-        }
-        if (request.content == null || request.content.isBlank()) {
-            throw new IllegalArgumentException("내용은 필수입니다!");
-        }
+        postValidator.PostCreateValidation(request);
+
         String createdAt = java.time.LocalDateTime.now().toString();
         Post post = new Post(postRepository.generateId(), request.title, request.content, request.author, createdAt);
         postRepository.save(post);
+
         return new CreatePostResponse(post.getId(), "게시글 등록 완료!");
     }
 
     // READ - 전체 📝 과제
     public List<PostResponse> getAllPosts() {
         List<Post> posts = postRepository.findAll();
+
         return posts.stream()
                 .map(PostResponse::new)
                 .collect(Collectors.toList());
@@ -38,26 +38,22 @@ public class PostService {
     // READ - 단건 📝 과제
     public PostResponse getPost(Long id) {
         Post post = postRepository.findById(id);
-        if(post == null) {
-            throw new PostNotFoundException(id);
-        }
+        postValidator.PostExistValidation(post, id);
+
         return new PostResponse(post);
     }
 
     // UPDATE 📝 과제
     public void updatePost(Long id, String newTitle, String newContent) {
         Post post = postRepository.findById(id);
-        if(post == null) {
-            throw new PostNotFoundException(id);
-        }
+        postValidator.PostExistValidation(post, id);
         post.update(newTitle, newContent);
     }
 
     // DELETE 📝 과제
     public void deletePost(Long id) {
-        boolean isDeleted = postRepository.deleteById(id);
-        if(!isDeleted) {
-            throw new PostNotFoundException(id);
-        }
+        Post post = postRepository.findById(id);
+        postValidator.PostExistValidation(post, id);
+        postRepository.deleteById(id);
     }
 }
