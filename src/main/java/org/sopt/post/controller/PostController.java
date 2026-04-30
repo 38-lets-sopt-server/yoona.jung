893a99a26d2a1c1ee3c7ceb9d02e09dd4b6efce5
laura-jung <166522604+laura-jung@users.noreply.github.com>
2026-04-30 15:25:45 +0900

    feat: post에 swagger 적용

--- src/main/java/org/sopt/post/controller/PostController.java
+++ src/main/java/org/sopt/post/controller/PostController.java
@@ -1,5 +1,10 @@
 package org.sopt.post.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.sopt.global.common.dto.BaseResponse;
 import org.sopt.post.dto.request.CreatePostRequest;
 import org.sopt.post.dto.request.UpdatePostRequest;
@@ -12,8 +17,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Post", description = "게시글 api")
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("api/v1/posts")
 public class PostController {
     private final PostService postService;
 
@@ -21,7 +27,14 @@ public class PostController {
         this.postService = postService;
     }
 
-    // POST /posts
+    @Operation(
+            summary = "게시글 생성",
+            description = "게시글을 생성합니다. "
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "생성 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력값")
+    })
     @PostMapping
     public ResponseEntity<BaseResponse<CreatePostResponse>> createPost(@RequestBody CreatePostRequest request) {
         CreatePostResponse response = postService.createPost(request);
@@ -29,30 +42,67 @@ public class PostController {
                 .body(BaseResponse.success("게시글 등록 성공", response));
     }
 
+    @Operation(
+            summary = "게시글 전체 조회",
+            description = "게시글 전체를 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없음")
+    })
     @GetMapping
     public ResponseEntity<BaseResponse<List<PostResponse>>> getAllPosts() {
         List<PostResponse> response = postService.getAllPosts();
         return ResponseEntity.ok(BaseResponse.success("게시글 전체 조회 성공", response));
     }
 
+    @Operation(
+            summary = "게시글 단건 조회",
+            description = "게시글 ID로 특정 게시글을 조회합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "조회 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없음")
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<BaseResponse<PostResponse>> getPost(@PathVariable Long id) {
+    public ResponseEntity<BaseResponse<PostResponse>> getPost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
+            @PathVariable Long id
+    ) {
         PostResponse response = postService.getPost(id);
         return ResponseEntity.ok(BaseResponse.success("게시글 조회 성공", response));
     }
 
-    // PUT /posts/{id}
+    @Operation(
+            summary = "게시글 수정",
+            description = "입력한 ID의 게시글의 제목과 내용을 수정합니다."
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "수정 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없음")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<BaseResponse<Void>> updatePost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
             @PathVariable Long id,
             @RequestBody UpdatePostRequest request) {
         postService.updatePost(id, request);
         return ResponseEntity.ok(BaseResponse.success("게시글 수정 성공"));
     }
 
-    // DELETE /posts/{id}
+    @Operation(
+            summary = "게시글 삭제",
+            description = "게시글 ID에 해당하는 게시글을 삭제합니다. "
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "삭제 성공"),
+            @ApiResponse(responseCode = "404", description = "게시글을 찾을 수 없음")
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<BaseResponse<Void>> deletePost(@PathVariable Long id) {
+    public ResponseEntity<BaseResponse<Void>> deletePost(
+            @Parameter(description = "조회할 게시글의 ID", example = "1", required = true)
+            @PathVariable Long id
+    ) {
         postService.deletePost(id);
         return ResponseEntity.ok(BaseResponse.success("게시글 삭제 성공"));
     }
--- src/main/java/org/sopt/post/dto/request/UpdatePostRequest.java
+++ src/main/java/org/sopt/post/dto/request/UpdatePostRequest.java
@@ -1,6 +1,11 @@
 package org.sopt.post.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UpdatePostRequest(
+        @Schema(description = "게시글 수정 제목 ", example = "수정된 게시글 제목입니다.")
         String title,
+
+        @Schema(description = "게시글 수정 내용", example = "수정된 게시글 내용입니다.")
         String newContent
 ) {}
